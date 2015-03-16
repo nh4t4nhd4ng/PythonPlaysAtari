e47ac38ceb869dde4d50a33cbdf549bf0ac034f9
WillBrennan <william.brennan@kcl.ac.uk>
2015-03-16 15:08:24 +0000

    Added file for support scripts

---
+++ tools/scripts.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Will Brennan'
+
+# built-in libarys
+import logging
+# Standard libaries
+# Custom libaries
+
+logger = logging.getLogger('main')
+
+
